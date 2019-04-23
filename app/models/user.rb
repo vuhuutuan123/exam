561da1c6494135fefb6f9700653bf9f480f50c7e
PhuongNguyen258 <n.phuong2508@gmail.com>
2019-04-23 11:26:59 +0700

    commit 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :user_tests
-  has_many :tests 
-  #, through :user_tests
+  # has_many :user_tests
+  # has_many :tests , through :user_tests
+  has_and_belongs_to_many :tests
   has_secure_password
 end
--- app/models/user_test.rb
+++ app/models/user_test.rb
@@ -1,7 +1,7 @@
 class UserTest < ApplicationRecord
+  # belongs_to :test
+  # belongs_to :user
   belongs_to :result
-  belongs_to :test
-  belongs_to :user
   has_many :user_answers
   has_many :answers , through :user_answers
 end
