59b56fa5d6db5067563b107abb179e6c4447eea9
PhuongNguyen258 <n.phuong2508@gmail.com>
2019-05-30 14:09:07 +0700

    edit show done test

--- app/controllers/user/user_tests_controller.rb
+++ app/controllers/user/user_tests_controller.rb
@@ -2,6 +2,7 @@ class User::UserTestsController < ApplicationController
   layout 'user/application'
   before_action :authenticate_user!
 
+
   def new
     @user_test = UserTest.new
     @user_test.user_answers.build
@@ -9,12 +10,12 @@ class User::UserTestsController < ApplicationController
   end
 
   def show
-    @user_test = current_user.user_tests.all.find_by(id: params[:id]) 
-    @test = Test.find_by(id: params[:test]) 
+    @user_test = current_user.user_tests.find_by(id: params[:id]) 
+    @user_answer = UserAnswer.find_by(id: params[:id]) 
   end
 
   def index
-    @user_tests = current_user.user_tests.all 
+    @user_tests = current_user.user_tests
   end
 
   def create
@@ -33,4 +34,5 @@ class User::UserTestsController < ApplicationController
     params.require(:user_test).permit(:test_id, :user_id, 
       user_answers_attributes: [:question_id, :answer_id])
   end
+
 end
